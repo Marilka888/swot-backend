856ae3e5ff87a8dfd16830844b2390f26d7d7daa
Marilka888 <marilka888@mail.ru>
2025-04-21 23:55:23 +0300

    fix

--- src/main/java/ru/marilka/swotbackend/config/WebSocketConfig.java
+++ src/main/java/ru/marilka/swotbackend/config/WebSocketConfig.java
@@ -9,15 +9,15 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
-    }
-
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS(); // ws + fallback
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic"); // /topic/sessionUpdates
+        registry.setApplicationDestinationPrefixes("/app");
     }
 }
+
--- src/main/java/ru/marilka/swotbackend/controller/WebSocketController.java
+++ src/main/java/ru/marilka/swotbackend/controller/WebSocketController.java
@@ -1,16 +1,24 @@
 package ru.marilka.swotbackend.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import ru.marilka.swotbackend.model.FactorMessage;
 
 @Controller
+@RequiredArgsConstructor
 public class WebSocketController {
 
-    @MessageMapping("/update") // from frontend: /app/update
-    @SendTo("/topic/updates") // broadcast to: /topic/updates
-    public String broadcastUpdate(String message) {
-        return message;
-    }
+        private final SimpMessagingTemplate messagingTemplate;
+
+        @MessageMapping("/factor.add") // из фронта: /app/factor.add
+        public void addFactor(FactorMessage message) {
+            // сохранить в базу и т.д.
+            messagingTemplate.convertAndSend("/topic/factors/" + message.getSessionId(), message);
+        }
+
+
 }
 
